40350efbb82cdab1f20f30c7a760a4bfc4887be2
Your Name <you@example.com>
2022-03-27 17:53:10 +0200

    moved analytics to app outside

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import Intro from "./components/intro/intro";
 import RequestInfo from "./components/request-info/request-info";
 import ReactGA from "react-ga";
 
+ReactGA.initialize('G-66H7HBY29N');
+ReactGA.pageview(window.location.pathname + window.location.search);
+
 function App() {
-    ReactGA.initialize('G-66H7HBY29N');
-    ReactGA.pageview(window.location.pathname + window.location.search);
+
 
   const submitForm = () => console.log('submitted');
 
